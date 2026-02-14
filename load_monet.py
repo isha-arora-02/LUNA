201a5a6f0bb1dc5e3169facd86df397a492f827b
isha-arora-02 <iarora02@stanford.edu>
2026-02-14 07:51:33 -0800

    not sure whats happening here lol

--- load_monet.py
+++ load_monet.py
@@ -1,5 +1,28 @@
-from transformers import AutoProcessor, AutoModelForZeroShotImageClassification
+import torch
+import clip
+
+
+def get_transform(n_px):
+    def convert_image_to_rgb(image):
+        return image.convert("RGB")
+    return T.Compose(
+        [
+            T.Resize(n_px, interpolation=T.InterpolationMode.BICUBIC),
+            T.CenterCrop(n_px),
+            convert_image_to_rgb,
+            T.ToTensor(),
+            T.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+        ]
+    )
+
 
 # Load MONET model
-processor = AutoProcessor.from_pretrained("suinleelab/monet")
-model = AutoModelForZeroShotImageClassification.from_pretrained("suinleelab/monet")
+device = "cuda:0"
+
+model, preprocess = clip.load("ViT-L/14", device=device, jit=False)[0], get_transform(n_px=224)
+model.load_state_dict(torch.hub.load_state_dict_from_url("https://aimslab.cs.washington.edu/MONET/weight_clip.pt"))
+
+# Load MONET model - not sure how to get weights from huggingface
+# from transformers import AutoProcessor, AutoModelForZeroShotImageClassification
+# processor = AutoProcessor.from_pretrained("suinleelab/monet")
+# model = AutoModelForZeroShotImageClassification.from_pretrained("suinleelab/monet")
