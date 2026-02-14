2d22ca1fe849990b7cdc388a653faea85c56cfa7
isha-arora-02 <iarora02@stanford.edu>
2026-02-14 10:58:29 -0800

    transcriptomics encoder model upload

--- load_monet.py
+++ load_monet.py
@@ -1,5 +1,6 @@
 from transformers import AutoProcessor, AutoModelForZeroShotImageClassification
 import torch
+from torch import nn
 import torch.nn.functional as F
 
 processor = AutoProcessor.from_pretrained("suinleelab/monet")
@@ -39,6 +40,21 @@ def get_img_embeddings(images: list, device='cpu'):
     embeddings = embeddings_tmp.pooler_output
     embeddings = F.normalize(embeddings, dim=1)
 
+# does the deseq?
+class transcriptomics_encoder(nn.Module):
+    def __init__(self, input_len=20000, first_layer_dim=768, out_dim=128):
+        super(transcriptomics_encoder, self).__init__()
+
+        self.nnlayers = nn.Sequential(
+            nn.Linear(first_layer_dim, 512),
+            nn.BatchNorm1d(512)
+        )
     
+    def forward(self, X):
+        self.nnlayers(X)
+
+
+
+        
 
 
